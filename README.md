736b093a0e153cfee7c459b8551ad1b82c4500a1
Mark Kropf <markkropf@gmail.com>
2023-10-21 00:23:51 -0400

    yay, it works!

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bizcard-image-merge
+
+I used this command in bash to clip the first two lines of all the svg files. This was needed since pdfkit crashes hard on the first xml lines of the file.
+
+```bash
+ls *.svg | xargs sed -i '' '1,2d'
+```
+
+later I learned that pdflib also does not like html comments in svg's
+
+```bash
+ls *.svg | xargs sed -i '' '2d'
+```
--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const mergeImages = require('merge-images');
-const { Canvas, Image } = require('canvas');
+const express = require('express');
+const { writeFileSync, readFileSync } = require("fs");
+const { degrees, PDFDocument, StandardFonts, rgb } = require("pdf-lib");
 
-mergeImages(['/images/dark_back.png', '/images/qr-codes/qr_0001.png'], {
-  Canvas: Canvas,
-  Image: Image
-})
-  .then(b64 => document.querySelector('img').src = b64);
-  // data:image/png;base64,iVBORw0KGgoAA..
+const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', async function(req, res) {
+  var qrCounter = "0001";
+  var svg = firstPage = pages = pdfBytes = pdfDoc = null;
+  const qrPath = "./public/qr-codes/";
+  const savePath = "./public/businesscards/";
+  for (let i = 250; i < 500; i++) {
+    pdfDoc = await PDFDocument.load(readFileSync('./public/pdf/light.pdf'));
+    var incrementvalue = (+qrCounter) +i;
+    incrementvalue = ("0000" + incrementvalue).slice(-4);
+    var filename = "qr_" + incrementvalue;
+    var svg = await readFileSync(qrPath+filename+".svg").toString();
+    console.log("Import: " + qrPath + filename + ".svg");
+    var pages = pdfDoc.getPages()
+    var firstPage = pages[0]
+    //light
+    firstPage.drawSvgPath(svg, {x:103.5, y: 58.5, color: rgb(.145,.129,.345), scale: .043 });
+    //dark 
+    //firstPage.drawSvgPath(svg, {x:103.5, y: 58.5, color: rgb(1,.98,.933), scale: .043 });
+    var pdfBytes = await pdfDoc.save()
+    writeFileSync(savePath+filename+".pdf", await pdfDoc.save());
+    console.log("Export: " + savePath + filename + ".pdf");
+    svg = firstPage = pages = pdfBytes = pdfDoc = null;
+  }
+  res.send("success!");
+});
+
+app.listen(3000, () => {
+    console.log('listening to port 3000');
+  }
+  );
